a3b5b0ada1e149f96d6b0e0a07cf1685aa8dc058
COOLIGHT <810813193@qq.com>
2021-05-17 23:02:44 +0800

    创建类和方法

---
+++ src/main/java/teachercasemanagementsystem/umlgroup/controller/RegisterController.java
@@ -0,0 +1,16 @@
+package teachercasemanagementsystem.umlgroup.controller;
+/*
+ *   @Author:COOLIGHT
+ *   @Ide:IntelliJ IDEA
+ *   @Creation time:2021/5/14 0014 11:01
+ */
+
+public class RegisterController {
+
+    /*
+    * 验证登录信息合法性
+    * @return 合法返回true,否则返回false*/
+    public boolean checkRegister() {
+        return false;
+    }
+}
---
+++ src/main/java/teachercasemanagementsystem/umlgroup/controller/SubmitCaseController.java
@@ -0,0 +1,12 @@
+package teachercasemanagementsystem.umlgroup.controller;
+/*
+ *   @Author:COOLIGHT
+ *   @Ide:IntelliJ IDEA
+ *   @Creation time:2021/5/17 0017 22:52
+ */
+
+public class SubmitCaseController {
+    public int addCaseInfo(){
+        return 0;
+    }
+}
