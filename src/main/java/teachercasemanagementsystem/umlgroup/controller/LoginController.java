e11f3722ae9707639e05db2d37384da327ebf854
COOLIGHT <810813193@qq.com>
2021-06-09 17:06:40 +0800

    实现验证注册信息合法后做出响应

---
+++ src/main/java/teachercasemanagementsystem/umlgroup/controller/LoginController.java
@@ -0,0 +1,36 @@
+package teachercasemanagementsystem.umlgroup.controller;
+/*
+ *   @Studio:CHJY · 源梦科技
+ *   @Author:COOLIGHT
+ *   @Ide:IntelliJ IDEA
+ *   @Creation time:2021/6/9 0009 17:00
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class LoginController {
+    public boolean verifyLoginInfo(String teacherId) {
+        Connection conn;
+        PreparedStatement pstmt;
+        String sql = "select teacher_Id from teacher where teacher_id = ?";
+        ResultSet rs;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("DB_URL","USER","PASS");//缺失--数据库连接url,角色
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,teacherId);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                return true;
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
